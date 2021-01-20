2a1bc52592d16b0298a980564a5b25d20a77f596
maxFZ <maksym2234@gmail.com>
2021-01-21 01:07:42 +0200

    add questions

--- ui.py
+++ ui.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class  QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz:QuizBrain):
+        self.quiz = quiz
         self.window = Tk()
         self.window.title('Quizzler')
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -16,6 +18,7 @@ class  QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
+            width=280,
             text='SOME TEXT',
             fill=THEME_COLOR,
             font=("Arial", 20, "italic"))
@@ -24,10 +27,15 @@ class  QuizInterface:
         true_image = PhotoImage(file='images/true.png')
         false_image = PhotoImage(file='images/false.png')
         self.true_button = Button(image=true_image)
-        self.true_button.grid(row=2, column=0, higlightthickness=0)
+        self.true_button.grid(row=2, column=0)
         self.false_button = Button(image=false_image)
-        self.false_button.grid(row=2, column=1, higlightthickness=0)
+        self.false_button.grid(row=2, column=1)
 
-
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text )
+
