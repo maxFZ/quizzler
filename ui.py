4f459de688381a1966914b59e8398af5917cd36a
maxFZ <maksym2234@gmail.com>
2021-01-21 19:30:01 +0200

    add button functionality

--- ui.py
+++ ui.py
@@ -26,9 +26,9 @@ class  QuizInterface:
 
         true_image = PhotoImage(file='images/true.png')
         false_image = PhotoImage(file='images/false.png')
-        self.true_button = Button(image=true_image)
+        self.true_button = Button(image=true_image, command=self.true_pressed)
         self.true_button.grid(row=2, column=0)
-        self.false_button = Button(image=false_image)
+        self.false_button = Button(image=false_image, command=self.false_pressed)
         self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
@@ -36,6 +36,29 @@ class  QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text )
+        self.canvas.config(bg='white')
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f'Score: {self.quiz.score}')
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text )
+        else:
+            self.canvas.itemconfig(self.question_text, text='empty list of questions')
+            self.true_button.config(state='disabled')
+            self.false_button.config(state='disabled')
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer('False')
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg='green')
+        else:
+            self.canvas.config(bg='red')
+        self.window.after(1000, self.get_next_question)
+        
+
 
